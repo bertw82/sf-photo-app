49f755dedfcc41ee15f2bb6f4eccf669da9eb19e
Bert Witzel <bwitzel82@gmail.com>
2022-03-08 18:18:53 -0500

    add previous project files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import apiKey from './config';
+import userId from './userId';
 
 function App() {
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&user_id=${userId}&format=json&nojsoncallback=1`;
+
+  const [post, setPosts] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Somthing went wrong!');
+        }
+      })
+  }, []);
+
+  function Post(props) {
+    const { id, secret, server, title } = props.data;
+    return (
+      <li key={id}>
+        <img src={`https://live.staticflickr.com/${server}/${id}_${secret}.jpg`} alt={title} />
+      </li>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    error ? <h1>{error}</h1>
+    : <div></div>
   );
 }
 
