d9293cc8b619eb91e2980ac6d1a83c086d201a62
Bert Witzel <bwitzel82@gmail.com>
2022-03-26 17:02:03 -0400

    changes to Pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,7 +12,6 @@ function Pagination({
 
   const [currentPage, setCurrentPage] = useState(1);
   const [pictures, setPictures] = useState('');
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // function to show the pictures on the page, limited to the dataLimit (currently 9)
@@ -28,7 +27,6 @@ function Pagination({
     }
     // show the pictures for the current page
     showData(currentPage);
-    setLoading(false);
     // Scroll to the top of the page. I picked this up from this tutorial: https://academind.com/tutorials/reactjs-pagination
     window.scrollTo({ behavior: 'smooth', top: '0px' }); 
   }, [currentPage, data, dataLimit])
@@ -57,24 +55,29 @@ function Pagination({
         <h1>Uh Oh! Something is not working!</h1>
       </div>
     ); 
-  } else if (loading) {
-    return (
-      <h1>Loading...</h1>
-    )
-  } else {
-    return (
-      <div className="dataContainer">
-        <h1>{title}</h1>
-        <button className="home-button"><Link to="/home">Home</Link></button>
-        <ul className="pictureList">
-          {pictures}
-        </ul>
-        <ul className="buttonList">
-          {addPagination()}
-        </ul>
-      </div>
-    )
-  }
+  } 
+
+  return (
+    <>
+      {
+        pictures.length > 0 ?
+        (
+        <div className="dataContainer">
+          <h1>{title}</h1>
+          <button className="home-button"><Link to="/home">Home</Link></button>
+          <ul className="pictureList">
+            {pictures}
+          </ul>
+          <ul className="buttonList">
+            {addPagination()}
+          </ul>
+        </div>
+        ) : (
+          <h1 className="navy">No results at the moment...</h1>
+        )
+      }
+    </>
+  )
 }
     
 
